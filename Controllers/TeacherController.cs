bf606d6b65504ae36112df6048668802f2e5643c
dbotoc <botocdavid@gmail.com>
2023-08-21 21:47:02 +0300

    Add project files.

---
+++ Controllers/TeacherController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using mvc_search.Models;
+
+namespace mvc_search.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private mvcEntities db = new mvcEntities();
+
+        // GET: Teacher
+        public async Task<ActionResult> Index(string searchValue)
+        {
+            var result = await db.Teachers.Where(s => s.Name.ToLower().Contains(searchValue.ToLower()) || s.Description.ToLower().Contains(searchValue.ToLower())).ToListAsync();
+
+            if (!string.IsNullOrEmpty(searchValue))
+                return View(result);
+            return View(await db.Teachers.ToListAsync());
+        }
+
+        // GET: Teacher/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Teacher teacher = await db.Teachers.FindAsync(id);
+            if (teacher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(teacher);
+        }
+
+        // GET: Teacher/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Teacher/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name,Age,Description")] Teacher teacher)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Teachers.Add(teacher);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(teacher);
+        }
+
+        // GET: Teacher/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Teacher teacher = await db.Teachers.FindAsync(id);
+            if (teacher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(teacher);
+        }
+
+        // POST: Teacher/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,Age,Description")] Teacher teacher)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(teacher).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(teacher);
+        }
+
+        // GET: Teacher/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Teacher teacher = await db.Teachers.FindAsync(id);
+            if (teacher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(teacher);
+        }
+
+        // POST: Teacher/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Teacher teacher = await db.Teachers.FindAsync(id);
+            db.Teachers.Remove(teacher);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
